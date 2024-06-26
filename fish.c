47c66473e9b35a797f1527d7331bf50d65ec6637
gheux <ialm0s9@outlook.com>
2024-06-26 22:18:45 +0800

    Modify fish.c

--- fish.c
+++ fish.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Certainly employee simple ever front trip.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
