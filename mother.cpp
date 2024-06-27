9f351d87b2b48303a8672a8adf88ab8b85f5621c
gheux <ialm0s9@outlook.com>
2024-06-27 22:51:22 +0800

    Modify mother.cpp

--- mother.cpp
+++ mother.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Center side career role.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
