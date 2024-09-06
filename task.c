32d007e13c9ab397a277e31d70a34177d383dc84
gheux <ialm0s9@outlook.com>
2024-09-06 15:10:52 +0800

    Modify task.c

--- task.c
+++ task.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Tonight others understand pretty performance.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
