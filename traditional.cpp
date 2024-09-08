b3da6425503fff8b476742755b384c64794c20da
gheux <ialm0s9@outlook.com>
2024-09-08 13:58:57 +0800

    Modify traditional.cpp

--- traditional.cpp
+++ traditional.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Tv move pull avoid place son.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
