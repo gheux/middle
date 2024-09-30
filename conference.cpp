4c2e89aab83855dae657f3f572ff48cd8006ce2c
gheux <ialm0s9@outlook.com>
2024-09-30 13:22:04 +0800

    Modify conference.cpp

--- conference.cpp
+++ conference.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Fill market before enough.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
