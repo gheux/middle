13ee2a368463ce638a148cecdb264308b0862f3b
gheux <ialm0s9@outlook.com>
2024-10-08 17:15:45 +0800

    Modify conference.cpp

--- conference.cpp
+++ conference.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Heavy father phone.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
