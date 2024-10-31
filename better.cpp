aaffbc247f7b1c66f138c3ee2844772f11ee9cfa
gheux <ialm0s9@outlook.com>
2024-10-31 13:13:15 +0800

    Modify better.cpp

--- better.cpp
+++ better.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Reflect music speech truth.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
