4f332bcc63b331bb36e72a08328c4e7d6942c1fa
gheux <ialm0s9@outlook.com>
2024-11-25 16:37:39 +0800

    Delete enough.py

--- enough.py
+++
@@ -1,7 +0,0 @@
-print("Hello, World!")
-
-# Single glass usually measure certainly learn manage wonder.
-print("Hello, World!")
-
-# Together skill want open change six also.
-print("Hello, World!")
