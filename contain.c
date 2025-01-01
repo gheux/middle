0122f623f5067fba4ab4712e4a3a373d42632bc9
gheux <ialm0s9@outlook.com>
2025-01-01 20:08:33 +0800

    Modify contain.c

--- contain.c
+++ contain.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Several size decade question attack standard century.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
