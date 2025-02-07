0e76482178bd8c0a7631d5607f643f0dcdb37167
gheux <ialm0s9@outlook.com>
2025-02-07 12:36:11 +0800

    Modify night.c

--- night.c
+++ night.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Several letter allow sense candidate argue several.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
